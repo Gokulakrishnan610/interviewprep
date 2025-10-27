ee68e605c2f6ba52677a95c02dd9ba5d649d1775
MULTidll <multimediamukesh@gmail.com>
2025-10-28 00:54:10 +0530

    Update Firebase configuration with hardcoded values

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAHr3_b9R6Y1QOqQXmdsRPkhvM_PYSlMxw",
+  authDomain: "prep-edb88.firebaseapp.com",
+  projectId: "prep-edb88",
+  storageBucket: "prep-edb88.firebasestorage.app",
+  messagingSenderId: "583850355165",
+  appId: "1:583850355165:web:f3912f232f111f7d54ae34",
+  measurementId: "G-TW34N9HPPH"
 };
 
 // Initialize Firebase
